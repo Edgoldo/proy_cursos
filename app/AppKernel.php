8b8c6f09d149b94de5136c85c0d6efe64579f57b
Edgoldo <edgetrono@gmail.com>
2017-07-31 23:10:35 -0400

    Se incluye LiuggioExcelBundle para generar reportes de los diferentes listados a través de archivos

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new CursosBundle\CursosBundle(),
+            new Liuggio\ExcelBundle\LiuggioExcelBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
